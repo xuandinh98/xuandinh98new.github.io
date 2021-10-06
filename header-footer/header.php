5bf545485516c51989e3ecf32e44dfbe5ed0011b
maithiquynhnhu <85109855+maithiquynhnhu@users.noreply.github.com>
2021-10-06 23:58:37 +0700

    TEST

---
+++ header-footer/header.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>GONGFIX</title>
+        <link rel="stylesheet" href="header.css">
+        <link rel="stylesheet" href="themify-icons/themify-icons.css">
+    </head>
+    <body>
+        <div id="header">
+        <div id="nav-line">
+        <div id=icon></div>
+        <div id="menu">
+        <ul>
+            <li>HOME</li>
+            <li>GIỚI THIỆU</li>
+            <li>CỬA HÀNG</li>
+            <li>SỰ KIỆN</li>
+            <li>ĐỐI TÁC</li>
+        </ul>
+        </div>
+        </div>
+        <div id="button-line">
+        <input id="search" type="text" placeholder="Search"><i class="ti-search" style="margin-right:50px;   cursor: pointer;"></i>
+        <button class="snip1582">Đăng nhập</button>
+        <button class="snip1582">Đăng ký</button>
+        </div>
+        <div class="clear"></div>
+        </div>
+    </body>
+</html>
---
+++ header-footer/home.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>GONGFIX</title>
+        <link rel="stylesheet" href="home.css">
+    </head>
+    <body>
+    <video id="video_background" preload="auto" autoplay="true" loop="loop" muted volume="0">
+    <source src="video/background.mp4" type="video/webm"/>
+    </video>
+    <div id="btn-anchor">
+    <button>GONGFIX</button>
+    <button>DỊCH VỤ</button>
+    <button>SỰ KIỆN</button>
+    <button>LIÊN HỆ</button>
+    </div>
+    </body>
+</html>
